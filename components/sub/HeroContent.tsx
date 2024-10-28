ff84c79a2170d543e443dc964bd8bdf2614bbf64
Euler <pengeuler@gmail.com>
2024-10-28 20:42:27 +0800

    333

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -64,7 +64,7 @@ const HeroContent = () => {
         </motion.p>
         <motion.a
           href="/more"
-          tarket="_blank"
+          target="_blank"
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
